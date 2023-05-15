ffb2c2aefe3a3a08924ba90eb7e08b340154f16a
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-15 23:55:39 +0600

    Codeforces Practice Sheet Contest 2 (C)

---
+++ C_Finding_Minimums.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n,k,x,mn = INT_MAX;
+    scanf("%d %d",&n,&k);
+    int rem = n%k;
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d",&x);
+        if(x<mn) mn = x;
+        if(i%k==0)
+        {
+            printf("%d ",mn);
+            mn = INT_MAX;
+        }
+        else if(i==n)
+        {
+            printf("%d",mn);
+        }
+    }
+    
+    
+    
+    return 0;
+}
